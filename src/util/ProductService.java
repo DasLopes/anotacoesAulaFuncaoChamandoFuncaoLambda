bc15bcece085738e55a7aea7aa3c74aca775f817
daslopes <daslopes@outlook.com.br>
2024-05-21 15:11:49 -0300

    Classe ProductService recebe classe Product como argumento e retorna o resultado da soma

---
+++ src/util/ProductService.java
@@ -0,0 +1,16 @@
+package util;
+import entities.Product;
+import java.util.List;
+
+public class ProductService {
+
+    public double filteredSum(List<Product> list){
+        double sum = 0.0;
+        for (Product p : list) {
+            if(p.getName().charAt(0) == 'T'){
+                sum += p.getPrice();
+            }
+        }
+        return sum;
+    }
+}
